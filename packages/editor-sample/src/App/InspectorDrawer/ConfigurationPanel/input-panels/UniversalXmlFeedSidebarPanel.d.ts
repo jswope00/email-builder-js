ae1006e91d0a05bd8c0262a332c93c0fc5bb22d6
Nattusia <runa@sitiq.com>
2026-03-12 23:23:05 +0100

    fixings for deploy

--- packages/editor-sample/src/App/InspectorDrawer/ConfigurationPanel/input-panels/UniversalXmlFeedSidebarPanel.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { UniversalXmlFeedProps } from '@nattusia/block-xml-feed';
-type UniversalXmlFeedSidebarPanelProps = {
-    data: UniversalXmlFeedProps;
-    setData: (v: UniversalXmlFeedProps) => void;
-};
-export default function UniversalXmlFeedSidebarPanel({ data, setData, }: UniversalXmlFeedSidebarPanelProps): React.JSX.Element;
-export {};
-//# sourceMappingURL=UniversalXmlFeedSidebarPanel.d.ts.map
